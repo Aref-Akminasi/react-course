85700cfafe6dc1ecde38ad640705f39c429935f7
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-09-08 20:54:36 +0200

    added corrections

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import Button from '../../UI/Button/Button';
 import styles from './CourseInput.module.css';
 
@@ -19,9 +20,7 @@ const CourseInput = (props) => {
       setIsValid(false);
       return;
     }
-
     props.onAddGoal(enteredValue);
-    setEnteredValue('');
   };
 
   return (
@@ -30,11 +29,7 @@ const CourseInput = (props) => {
         className={`${styles['form-control']} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
-        <input
-          value={enteredValue}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
