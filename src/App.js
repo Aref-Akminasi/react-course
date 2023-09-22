fd1bb47c4be36c7f96e7943e9031681d85158241
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-09-22 14:57:46 +0200

    fixed fetch link issue

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-bd413-default-rtdb.firebaseio.com/tasks/json'
+        'https://react-http-bd413-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
